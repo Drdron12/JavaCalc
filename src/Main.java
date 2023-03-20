d19374e62f8cff363fff1ed855c14c940999a752
Drdron12 <66367182+Drdron12@users.noreply.github.com>
2023-03-20 21:13:19 +0600

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.util.Scanner;
+import java.lang.NumberFormatException;
 public class Main {
     public static void main(String[] args) {
         ArabicRome helper = new ArabicRome();
@@ -10,27 +11,57 @@ public class Main {
         String[] allowed_operations = {"+", "-", "/", "*"};
 
         for (String action : allowed_operations) {
-            if (str.contains(action)) {
+            if (!str.contains(action)){
+                try {
+                    throw new IOException();
+                } catch (IOException e) {
+                    System.out.println("Вы не ввели нужное действие между числами!");
+                    return;
+                }
+            }
+            else {
                 String[] numbers = str.split("[" + action + "]");
+                if (numbers.length != 2){
+                    try {
+                        throw new IOException();
+                    } catch (IOException e) {
+                        System.out.println("Нужно ввести два числа и действие между ними!");
+                        return;
+                    }
+                }
+
                 String a = numbers[0];
                 String b = numbers[1];
 
-                if (helper.isArabic(a) && !helper.isArabic(b) ){
-                    System.out.println("Одно из чисел арабское, а другое римское!\nТак нельзя");
-                    return;
+                if (helper.isArabic(a) != helper.isArabic(b) ){
+                    try {
+                        throw new IOException();
+                    } catch (IOException e) {
+                        System.out.println("Одно из чисел арабское, а другое римское!\nТак нельзя");
+                        return;
+                    }
                 }
 
                 boolean return_romanian = helper.isArabic(a) && helper.isArabic(b);
 
                 a = helper.RomanianToArabic(a);
                 b = helper.RomanianToArabic(b);
-
-                int a_int = Integer.parseInt(a);
-                int b_int = Integer.parseInt(b);
+                int a_int, b_int;
+                try {
+                    a_int = Integer.parseInt(a);
+                    b_int = Integer.parseInt(b);
+                } catch (NumberFormatException e) {
+                    System.out.println("Прописано неподходящее число");
+                    return;
+                }
 
                 if ( !(a_int >= 0 && a_int <= 10) && !(b_int >= 0 && b_int <= 10)){
-                    System.out.println("Оба числа должны быть в диапазоне от 0 до 10!");
-                    return;
+                    try {
+                        throw new IOException();
+                    } catch (IOException e) {
+                        System.out.println("Оба числа должны быть в диапазоне от 0 до 10!");
+                        return;
+                    }
                 }
 
                 int answer = 0;
@@ -62,6 +93,7 @@ public class Main {
                     return;
                 }
                 System.out.println(answer);
+                return;
             }
 
         }
